82473fca0a73aa1a21a6f96bdb5f6b26d0cd9ad6
Matheus_Oliveira_Cne <matheus@cnesistemas.com.br>
2025-11-18 16:17:09 -0300

    Inserting Exceptions Filters

---
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseValidator.cs
@@ -0,0 +1,15 @@
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+using FluentValidation;
+
+namespace CashFlow.Application.UseCases.Expenses.Register;
+public class RegisterExpenseValidator : AbstractValidator<RequestRegisterExpenseJson>
+{
+    public RegisterExpenseValidator()
+    {
+        RuleFor(expense => expense.Title).NotEmpty().WithMessage("Title Required");
+        RuleFor(expense => expense.Amount).GreaterThan(0).WithMessage("Amount must be greater than zero");
+        RuleFor(expense => expense.Date).LessThan(DateTime.UtcNow).WithMessage("Date cannot be in the future");
+        RuleFor(expense => expense.PaymentType).IsInEnum().WithMessage("Invalid payment type");
+    }
+}
