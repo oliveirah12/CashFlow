47a0a601735b06f2a1e9475f32013bad9c532882
Matheus_Oliveira_Cne <matheus@cnesistemas.com.br>
2025-11-21 10:21:45 -0300

    Finished Localization

--- src/CashFlow.API/Middleware/CultureMiddleware.cs
+++ src/CashFlow.API/Middleware/CultureMiddleware.cs
@@ -13,13 +13,17 @@ public class CultureMiddleware
 
     public async Task Invoke(HttpContext context)
     {
-        var culture = context.Request.Headers.AcceptLanguage.FirstOrDefault();
+        var supportedLanguages = CultureInfo.GetCultures(CultureTypes.AllCultures).ToList();
+
+        var requestedCulture = context.Request.Headers.AcceptLanguage.FirstOrDefault();
+
 
         var cultureInfo = new CultureInfo("en");
 
-        if(!string.IsNullOrWhiteSpace(culture))
+        if(!string.IsNullOrWhiteSpace(requestedCulture)
+            && supportedLanguages.Exists(language => language.Name.Equals(requestedCulture)))
         {
-            cultureInfo = new CultureInfo(culture);
+            cultureInfo = new CultureInfo(requestedCulture);
         }
 
         CultureInfo.CurrentCulture = cultureInfo;
