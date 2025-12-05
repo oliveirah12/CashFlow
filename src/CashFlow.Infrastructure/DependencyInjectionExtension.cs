e0d08b9530ab31bce96c9d527358f38100735cf4
Matheus_Oliveira_Cne <matheus@cnesistemas.com.br>
2025-12-05 11:39:58 -0300

    SOLID - Letter I

--- src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
+++ src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
@@ -19,7 +19,8 @@ public static class DependencyInjectionExtension
 
     private static void AddRepositories(IServiceCollection services)
     {
-        services.AddScoped<IExpensesRepository, ExpensesRepository>();
+        services.AddScoped<IExpensesReadOnlyRepository, ExpensesRepository>();
+        services.AddScoped<IExpensesWriteOnlyRepository, ExpensesRepository>();
         services.AddScoped<IUnitOfWork, UnitOfWork>();
     }
 
