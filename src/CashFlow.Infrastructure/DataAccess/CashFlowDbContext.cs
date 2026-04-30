179b93d5e49b0e867f3e1c4a716bbd52193e5fc9
Matheus_Oliveira_Cne <matheus-homem2001@hotmail.com>
2026-04-30 17:12:13 -0300

    Teste

--- src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
+++ src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -7,7 +7,6 @@ namespace CashFlow.Infrastructure.DataAccess;
 internal class CashFlowDbContext : DbContext
 {
     public CashFlowDbContext(DbContextOptions options) : base(options) { }
-
     public DbSet<Expense> Expenses { get; set; } 
     public DbSet<User> Users { get; set; }
     
