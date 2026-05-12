68a5c296c10e9c8d06cb1631a11a100ca8905d05
Matheus_Oliveira_Cne <matheus-homem2001@hotmail.com>
2026-05-12 15:15:30 -0300

    Using InMemory Database - #F240

--- src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
+++ src/CashFlow.Infrastructure/DependencyInjectionExtension.cs
@@ -45,8 +45,8 @@ public static class DependencyInjectionExtension
     private static void AddDbContext(IServiceCollection services, IConfiguration configuration)
     {
         var connectionString = configuration.GetConnectionString("Connection");
-        var version = new Version(8, 0, 37);
-        var serverVersion = new MySqlServerVersion(version);
+        
+        var serverVersion = ServerVersion.AutoDetect(connectionString);
 
         services.AddDbContext<CashFlowDbContext>(config => config.UseMySql(connectionString, serverVersion));
     }
