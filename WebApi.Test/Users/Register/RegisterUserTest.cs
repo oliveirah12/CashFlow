ee508a9b3ed1b5928453e78bf64bfa1e50cfe16e
Matheus_Oliveira_Cne <matheus-homem2001@hotmail.com>
2026-05-14 14:01:20 -0300

    First integration test - #F242

--- WebApi.Test/Users/Register/RegisterUserTest.cs
+++ WebApi.Test/Users/Register/RegisterUserTest.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Shouldly;
 using System.Net;
 using System.Net.Http.Json;
+using System.Text.Json;
 
 namespace WebApi.Test.Users.Register;
 
@@ -23,5 +24,11 @@ public class RegisterUserTest : IClassFixture<CustomWebApplicationFactory>
         var result = await _httpClient.PostAsJsonAsync(METHOD, request);
 
         result.StatusCode.ShouldBe(HttpStatusCode.Created);
+        var body = await result.Content.ReadAsStreamAsync();
+
+        var response = await JsonDocument.ParseAsync(body);
+
+        response.RootElement.GetProperty("name").GetString().ShouldBe(request.Name);
+        response.RootElement.GetProperty("token").GetString().ShouldNotBeNullOrEmpty();
     }
 }
