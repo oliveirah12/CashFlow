94dfc85bcec5dd7dbe8f9a0eb94c89af25d914c9
Matheus_Oliveira_Cne <matheus-homem2001@hotmail.com>
2026-06-02 17:28:44 -0300

    #F255

--- WebApi.Test/Expenses/Register/RegisterExpenseTest.cs
+++ WebApi.Test/Expenses/Register/RegisterExpenseTest.cs
@@ -2,5 +2,10 @@
 
 public class RegisterExpenseTest : IClassFixture<CustomWebApplicationFactory>
 {
-
+    private const string METHOD = "api/Expenses";
+    private readonly HttpClient _client;
+    public RegisterExpenseTest(CustomWebApplicationFactory factory)
+    {
+        _client = factory.CreateClient();
+    }
 }
